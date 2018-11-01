fb1786d69f5a2447168c28d9a968698c31a76174
jiseon0702 <dlwltjszcm@naver.com>
2018-11-01 15:45:58 +0900

    1_header

--- calcMatrix.c
+++ calcMatrix.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define ROWS 3
+#define COLS 3
 
 void printMatrix(int A[][COLS])
 {
